810f20d60e727d621132825541acbbdfcc0c8f6d
yogizogi7 <cek0707@naver.com>
2024-08-13 21:45:40 +0900

    (2024.8.13)실수를 저장하는 방법

--- Program/source.c
+++ Program/source.c
@@ -2,83 +2,31 @@
 
 void main()
 {
-#pragma region 변수의 이름 규칙
-/*
-	1. 변수의 경우 이미 중복된 변수의 이름을 허용하진 않지만,
-	변수의 이름을 대소문자로 구별하여 사용할 수 있습니다.
+#pragma region 오버플로우
+	// 특정한 자료형이 표현할 수 있는 최댓값의 범위를 넘어서 연산을 수행하는 과정
+	// 부호 없는 자료형에서도 똑같이 발생하며, 실수일 때 오버플로우가 발생하면 infinity라는 값으로 출력합니다.
+	//char data = 129;
+	//printf("%d\n",data);
+#pragma endregion
+#pragma region 언더플로우
+	//특정한 자료형이 표현할 수 있는 최솟값의 범위를 넘어서 연산을 수행하는 과정
+	//언더플로우는 부호 없는 자료형에서도 똑같이 발생하며, 실수일 때 언더플로우가 발생하면 0이라는 값으로 출력한다.
 
-	2. 변수의 이름으로 예약어를 사용할 수 없습니다.
-	ex) int int = 10;
-
-	3.변수의 이름은 숫자로 시작할 수 없습니다.
-
-	4. 변수의 이름으로 공백이 포함될 수 없습니다.
-
-	5. 변수의 이름으로 특수 기호는 "_"와 "$"만 허용합니다.
-	ex) int m_attack = 10;
-	    int m$attack = 10;
-		int m!attack = 10;
-*/
 
 #pragma endregion
-#pragma region 비트
 
-	/*
-	데이터를 나타내는 최소의 단위, 0또는1의 조합으로 논리 계산을 수행하는 단위이다.
-
-	
-	
-
-	*/
-#pragma region (10)진수를 (2)진수로 변환하는 과정
-
-	/*
-	10진수를 1이 될 때까지 계속 2로 나누어 준 다음 나눈 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
-	*/
-
+#pragma region 시프트 연산자
+	//비트의 위치를 좌우로 특정한 값만큼 이동하는 연산자입니다.
+	//int x = 3;
+	//
+	//printf("%d\n", x <<= 2); 
+	//printf("%d", x >>=1);
 #pragma endregion
-#pragma region (2)진수를 (10)진수로 변환하는 과정
-	/*
-	1. 0바이트에 2진수로 저장된 값을 2의 제곱으로 나타냅니다.
-
-	각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한 다음 각각의 비트를 모두 더하여 10진수로 나타냅니다.
-	*/
-#pragma endregion
-
-	//메모리는 비트 단위로 데이터를 저장할 수 있으며 1개의 비트에는 0 또는 1의 값만 저장할 수 있다.
-
-	//1111
-#pragma endregion
-#pragma region 비트 연산자
-	/*
-	비트 단위로 논리 연산을 수행하기 위해 사용하는 연산자입니다.
-	*/
-#pragma region AND 연산자
-	/* 두 개의 피연산자가 모두 1이면 1을 반환하는 연산자
-	char a = 10 ;
-	char b = 13;
-	printf("%d\n", a & b);
-	*/
-#pragma endregion
-#pragma region OR 연산자
-	// 두 개의 피연산자 중에 하나라도 1이 있다면 1을 반환하는 연산자 입니다.
-	int c = 9;
-	int d = 11;
-	//printf("%d", c | d);
-#pragma endregion
-#pragma region XOR 연산자
-	// 두 개의 피연산자가 서로 같으면 0을 반환하고 서로 다르면 1을 반환하는 연산자
-	int e = 7;
-	int f = 5;
-	//printf("%d", e^f);
-#pragma endregion
-#pragma region NOT 연산자
-	// 하나의 피 연산자를 반전시키는 연산자
-	int g = 20;
-	int h = 11;
-	//printf("%d", ~g);
-
-#pragma endregion
+#pragma region 실수저장법
+	//float a=3.14;
+	//printf("%f\n", a);
+	//표준 출력 함수로 실수를 출력할 때 4바이트 크기의 실수형 자료형은 소수점 이하 6자리까지 표기
+	// 8바이트 크기의 실수형 자료형은 소수점 이하 15자리까지 표기
 #pragma endregion
 
 }
